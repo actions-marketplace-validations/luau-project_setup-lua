ff6391e48b4cde4d83a9e3a9cbe28e53ae6a7145
luau-project <luau.project@gmail.com>
2025-11-09 18:43:34 -0300

    fix saving to / restoring from GitHub cache.

--- src/GitHubCacheService.ts
+++ src/GitHubCacheService.ts
@@ -1,3 +1,5 @@
+import { basename } from "node:path";
+import { cp, rm } from "node:fs/promises";
 import { saveCache, restoreCache } from "@actions/cache";
 import { GitHubInput } from "./Util/GitHubInput";
 import { ICacheService } from "./Util/ICacheService";
@@ -10,27 +12,62 @@ export class GitHubCacheService implements ICacheService {
         }
         return GitHubCacheService._instance;
     }
+    private getPathForGitHubCache(path: string): string {
+        return `setup-lua-cache-${basename(path)}`;
+    }
     useCache(): boolean {
         return GitHubInput.instance().getInputUseCache();
     }
     save(path: string, key: string): Promise<void> {
         return new Promise<void>((resolve, reject) => {
-            saveCache([path], key)
-                .then(_ => {
-                    resolve();
+            const pathForCache = this.getPathForGitHubCache(path);
+            cp(path, pathForCache, { force: true })
+                .then(() => {
+                    saveCache([pathForCache], key, undefined, true)
+                        .then(_ => {
+                            rm(pathForCache, { force: true })
+                                .then(resolve)
+                                .catch(rmErr => {
+                                    resolve();
+                                });
+                        })
+                        .catch(saveErr => {
+                            rm(pathForCache, { force: true })
+                                .then(() => {
+                                    reject(saveErr);
+                                })
+                                .catch(rmErr => {
+                                    resolve(saveErr);
+                                });
+                        });
                 })
                 .catch(reject);
         });
     }
     restore(path: string, primaryKey: string): Promise<void> {
         return new Promise<void>((resolve, reject) => {
-            restoreCache([path], primaryKey)
+            const pathForCache = this.getPathForGitHubCache(path);
+            restoreCache([pathForCache], primaryKey, undefined, undefined, true)
                 .then(cacheKey => {
                     if (cacheKey) {
-                        resolve();
+                        cp(pathForCache, path, { force: true })
+                            .then(() => {
+                                rm(pathForCache, { force: true })
+                                    .then(resolve)
+                                    .catch(rmErr => {
+                                        resolve();
+                                    });
+                            })
+                            .catch(reject);
                     }
                     else {
-                        reject(new Error(`There is no cache available for ${primaryKey}.`));
+                        rm(pathForCache, { force: true })
+                            .then(() => {
+                                reject(new Error(`There is no cache available for ${primaryKey}.`));
+                            })
+                            .catch(rmErr => {
+                                reject(new Error(`There is no cache available for ${primaryKey}.`));
+                            });
                     }
                 })
                 .catch(reject);
